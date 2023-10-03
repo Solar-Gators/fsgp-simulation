193081c9c36be5d7d49259e49afec8b58bfa0cb0
Jack Schedel <jackschedel@gmail.com>
2023-10-03 11:50:25 -0400

    overlapping -> picewise (still bad offsets)

--- strategy-simulation.go
+++ strategy-simulation.go
@@ -6,61 +6,74 @@ import (
 	"os"
 	"strconv"
 
-	// "gonum.org/v1/gonum/diff/fd"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
 )
 
 const (
-	piecewiseCount    = 10
-	segmentLength     = 1.0
 	initialGraphValue = 0.0
+	segmentLength     = 1.0
 )
 
 func main() {
 	args := os.Args[1:]
 
-	if len(args) != piecewiseCount {
-		fmt.Println("Invalid number of arguments")
+	// Define whether each segment is a parabola (true) or a line (false)
+	piecewiseFunctions := []bool{true, false, true, false, true}
+	lineCount := 0
+	parabolaCount := 0
+
+	for _, isParabola := range piecewiseFunctions {
+		if isParabola {
+			parabolaCount++
+		} else {
+			lineCount++
+		}
+	}
+
+	expectedArgCount := 1 + lineCount + parabolaCount*2 // 1 for initial value, lineCount for lines, parabolaCount*2 for parabolas
+
+	if len(args) != expectedArgCount {
+		fmt.Println("Expected argument count: ", expectedArgCount)
 		os.Exit(1)
 	}
 
 	var (
-		parabola = true
-		y        = initialGraphValue
-		xys      plotter.XYs
+		y   = initialGraphValue
+		xys plotter.XYs
 	)
 
-  for i := 0; i < piecewiseCount; i++ {
-    if parabola {
-      a, _ := strconv.ParseFloat(args[i], 64)
-      i++
-      if i < len(args) { // Add this check
-        b, _ := strconv.ParseFloat(args[i], 64)
-        c := y - a*math.Pow(segmentLength, 2) - b*segmentLength
+	argIndex := 0
+	xOffset := 0.0
+	for _, isParabola := range piecewiseFunctions {
+		if isParabola {
+			a, _ := strconv.ParseFloat(args[argIndex], 64)
+			argIndex++
+			b, _ := strconv.ParseFloat(args[argIndex], 64)
+			argIndex++
+			c := y - a*math.Pow(segmentLength, 2) - b*segmentLength
 
-        for j := 0.0; j <= segmentLength; j += 0.01 {
-          y = a*math.Pow(j, 2) + b*j + c
-          xys = append(xys, plotter.XY{j, y})
-        }
-      }
-    } else {
-      if i < len(args) { // Add this check
-        m, _ := strconv.ParseFloat(args[i], 64)
-        b := y - m*segmentLength
+			for j := 0.0; j <= segmentLength; j += 0.01 {
+				x := xOffset + j
+				y = a*math.Pow(j, 2) + b*j + c
+				xys = append(xys, plotter.XY{x, y})
+			}
+		} else {
+			m, _ := strconv.ParseFloat(args[argIndex], 64)
+			argIndex++
+			b := y - m*segmentLength
 
-        for j := 0.0; j <= segmentLength; j += 0.01 {
-          y = m*j + b
-          xys = append(xys, plotter.XY{j, y})
-        }
-      }
-    }
+			for j := 0.0; j <= segmentLength; j += 0.01 {
+				x := xOffset + j
+				y = m*j + b
+				xys = append(xys, plotter.XY{x, y})
+			}
+		}
+		xOffset += segmentLength
+	}
 
-    parabola = !parabola
-  }
-
-  p := plot.New()
+	p := plot.New()
 
 	lines, err := plotter.NewLine(xys)
 	if err != nil {
