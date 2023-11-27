3cf58b3381c8ffd61e3f72289b1680b5d1c9ad52
Jack Schedel <jackschedel@gmail.com>
2023-11-27 13:04:39 -0500

    solver working but needs input constraint

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,6 @@
-from concurrent.futures import ThreadPoolExecutor
 from scipy.optimize import minimize
 import subprocess
+import sys
 
 subprocess.run(["go", "build", "."])
 cli_program = "./strategy-simulation"
@@ -10,15 +10,17 @@ output = None
 def objective(x):
     global output
     # Call your CLI program with the current parameters
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(subprocess.run, [cli_program] + list(map(str, x)), capture_output=True, text=True) for _ in range(4)]
-    results = [future.result() for future in futures]
-    # aggregate the results
-    output = [result.stdout if result.stdout is not None else "" for result in results]
+    result = subprocess.run([cli_program] + list(map(str, x)), capture_output=True, text=True)
+    output = result.stdout if result.stdout is not None else ""
 
     # Parse the output and return the value to minimize
-    # We will minimize the Time Elapsed (s)
-    return sum(float(out.split("Time Elapsed (s):")[1].split("\n")[0]) for out in output)
+    # If the output is infinity, return the maximum float value
+    try:
+        time_elapsed = float(output.split("Time Elapsed (s):")[1].split("\n")[0])
+        return time_elapsed if time_elapsed != float('inf') else sys.float_info.max
+    except ValueError:
+        # Handle the case where the conversion to float fails
+        return sys.float_info.max
 
 # Constraint function
 def constraint(x):
@@ -27,29 +29,35 @@ def constraint(x):
     # Parse the output and return the value for the constraint
     # Constraint is the Energy Consumption (W)
     if output:
-        return 1000 - sum(float(out.split("Energy Consumption (W):")[1].split("\n")[0]) for out in output)
+        return 1000 - float(output.split("Energy Consumption (W):")[1].split("\n")[0])
     else:
         return 1000
 
 # Constraint function for initial and final velocity
 def velocity_constraint(x):
     global output
+    acceptable_difference = 0.5  # 0.5% or an absolute threshold for very small velocities
 
     # Parse the output and get the initial and final velocities
     if output:
-        initial_velocities = [float(out.split("Initial Velocity (m/s):")[1].split("\n")[0]) for out in output]
-        final_velocities = [float(out.split("Final Velocity (m/s):")[1].split("\n")[0]) for out in output]
+        initial_velocity = float(output.split("Initial Velocity (m/s):")[1].split("\n")[0])
+        final_velocity = float(output.split("Final Velocity (m/s):")[1].split("\n")[0])
 
-        # Calculate the difference in percentage
-        velocity_differences = [abs(initial_velocity - final_velocity) / initial_velocity * 100 for initial_velocity, final_velocity in zip(initial_velocities, final_velocities)]
+        # Check for initial velocity close to zero
+        if abs(initial_velocity) < 1e-8:  # A threshold for considering velocity as zero
+            # Use an absolute difference for very small velocities
+            velocity_difference = abs(final_velocity)
+        else:
+            # Calculate the difference in percentage for non-zero initial velocities
+            velocity_difference = abs(initial_velocity - final_velocity) / initial_velocity * 100
 
-        # Velocity difference should be within 0.5%
-        return 0.5 - max(velocity_differences)
+        # Velocity difference should be within the acceptable threshold
+        return acceptable_difference - velocity_difference
     else:
-        return 0.5
+        return acceptable_difference
 
 # Initial guess
-x0 = [1]*8
+x0 = [0.00001]*8
 
 # Define the constraints
 con1 = {'type': 'ineq', 'fun': constraint}
@@ -60,3 +68,4 @@ res = minimize(objective, x0, method='SLSQP', constraints=[con1, con2], options=
 
 # Print the optimal solution
 print(res.x)
+
