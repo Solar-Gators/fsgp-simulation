a053e96a66e12bf1128db766898c603752890dce
Jack Schedel <jackschedel@gmail.com>
2023-11-27 13:59:01 -0500

    temp commit to try windows

--- optimizer.py
+++ optimizer.py
@@ -2,70 +2,68 @@ from scipy.optimize import minimize
 import subprocess
 import sys
 
-subprocess.run(["go", "build", "."])
 cli_program = "./strategy-simulation"
-output = None
+
+# Call CLI program and return output
+def call_cli_program(x):
+    return subprocess.run([cli_program] + list(map(str, x)), capture_output=True, text=True).stdout
+
+# Cache to store the output for the current x to avoid redundant CLI calls
+output_cache = {}
+
+# Function to get the output from cache or CLI call
+def get_output(x):
+    # Convert x to a tuple to use it as a dictionary key (lists are not hashable)
+    x_tuple = tuple(x)
+    if x_tuple not in output_cache:
+        output_cache[x_tuple] = call_cli_program(x)
+    return output_cache[x_tuple]
 
 # Objective function
 def objective(x):
-    global output
-    # Call your CLI program with the current parameters
-    result = subprocess.run([cli_program] + list(map(str, x)), capture_output=True, text=True)
-    output = result.stdout if result.stdout is not None else ""
-
-    # Parse the output and return the value to minimize
-    # If the output is infinity, return the maximum float value
+    output = get_output(x)
     try:
         time_elapsed = float(output.split("Time Elapsed (s):")[1].split("\n")[0])
         return time_elapsed if time_elapsed != float('inf') else sys.float_info.max
     except ValueError:
-        # Handle the case where the conversion to float fails
         return sys.float_info.max
 
-# Constraint function
-def constraint(x):
-    global output
-
-    # Parse the output and return the value for the constraint
-    # Constraint is the Energy Consumption (W)
-    if output:
-        return 1000 - float(output.split("Energy Consumption (W):")[1].split("\n")[0])
-    else:
-        return 1000
+# Constraint function for energy consumption
+def constraint_energy(x):
+    output = get_output(x)
+    energy_consumption = float(output.split("Energy Consumption (W):")[1].split("\n")[0])
+    # Return non-negative if constraint is satisfied, negative otherwise
+    return 5000 - energy_consumption
 
 # Constraint function for initial and final velocity
-def velocity_constraint(x):
-    global output
-    acceptable_difference = 0.5  # 0.5% or an absolute threshold for very small velocities
+def constraint_velocity(x):
+    output = get_output(x)
+    acceptable_difference = 5.0  # 5% difference threshold
+    max_velocity = 40.0  # Maximum allowed velocity
+    initial_velocity = float(output.split("Initial Velocity (m/s):")[1].split("\n")[0])
+    final_velocity = float(output.split("Final Velocity (m/s):")[1].split("\n")[0])
 
-    # Parse the output and get the initial and final velocities
-    if output:
-        initial_velocity = float(output.split("Initial Velocity (m/s):")[1].split("\n")[0])
-        final_velocity = float(output.split("Final Velocity (m/s):")[1].split("\n")[0])
+    # Return negative if velocities are greater than max_velocity
+    if initial_velocity > max_velocity or final_velocity > max_velocity:
+        return -1  
 
-        # Check for initial velocity close to zero
-        if abs(initial_velocity) < 1e-8:  # A threshold for considering velocity as zero
-            # Use an absolute difference for very small velocities
-            velocity_difference = abs(final_velocity)
-        else:
-            # Calculate the difference in percentage for non-zero initial velocities
-            velocity_difference = abs(initial_velocity - final_velocity) / initial_velocity * 100
-
-        # Velocity difference should be within the acceptable threshold
-        return acceptable_difference - velocity_difference
-    else:
-        return acceptable_difference
+    velocity_difference = abs(initial_velocity - final_velocity) / max(initial_velocity, final_velocity) * 100
+    # Return non-negative if constraint is satisfied, negative otherwise
+    return acceptable_difference - velocity_difference
 
 # Initial guess
-x0 = [0.00001]*8
+x0 = [0.05]*8
 
 # Define the constraints
-con1 = {'type': 'ineq', 'fun': constraint}
-con2 = {'type': 'ineq', 'fun': velocity_constraint}
+con1 = {'type': 'ineq', 'fun': constraint_energy}
+con2 = {'type': 'ineq', 'fun': constraint_velocity}
 
 # Solve the optimization problem
-res = minimize(objective, x0, method='SLSQP', constraints=[con1, con2], options={'disp': True, 'maxiter': 100})
+res = minimize(objective, x0, method='SLSQP', constraints=[con1, con2], options={'disp': True, 'maxiter': 80})
 
-# Print the optimal solution
 print(res.x)
+print()
 
+# Clear output_cache to ensure fresh output for final call
+output_cache.clear()
+print(call_cli_program(res.x))
