e56d0a33d3dac979a204ba5eb2cc9804352e2e36
Jack Schedel <jackschedel@gmail.com>
2023-11-27 20:22:49 -0500

    mystic stuff working but need to add penalties

--- optimizer.py
+++ optimizer.py
@@ -2,7 +2,9 @@ import subprocess
 import sys
 from mystic.solvers import fmin
 from mystic.monitors import VerboseMonitor
-from mystic.penalty import quadratic_inequality
+from mystic.constraints import as_constraint, and_
+from mystic.symbolic import generate_constraint, generate_solvers, simplify
+
 
 cli_program = "./strategy-simulation.exe"
 
@@ -46,88 +48,62 @@ def constraint_energy(x):
     energy_consumption = float(
         output.split("Energy Consumption (W):")[1].split("\n")[0]
     )
-    # Return positive if constraint is satisfied and negative if not
+    # Return non-negative if constraint is satisfied, negative otherwise
     return 5000 - energy_consumption
 
 
 # Constraint function for initial and final velocity
 def constraint_velocity(x):
     output = get_output(x)
+
+    # in %
     acceptable_difference = 0.5
-    max_velocity = 40.0
+
+    max_velocity = 40.0  # Maximum allowed velocity
     initial_velocity = float(output.split("Initial Velocity (m/s):")[1].split("\n")[0])
     final_velocity = float(output.split("Final Velocity (m/s):")[1].split("\n")[0])
 
-    # Penalize if velocities are out of bounds
-    if (
-        initial_velocity < 0
-        or initial_velocity > max_velocity
-        or final_velocity < 0
-        or final_velocity > max_velocity
-    ):
+    # Check if either velocity is outside the acceptable range [0, max_velocity]
+    if initial_velocity < 0 or initial_velocity > max_velocity:
+        return -1
+    if final_velocity < 0 or final_velocity > max_velocity:
         return -1
 
-    # Penalize if the velocity difference percentage is too high
+    # Calculate the percentage difference between the initial and final velocities
     if initial_velocity == final_velocity == 0:
-        velocity_difference_percentage = 0
+        velocity_difference = 0
     else:
-        velocity_difference_percentage = (
+        # Otherwise, calculate the difference as a percentage
+        velocity_difference = (
             abs(initial_velocity - final_velocity)
             / max(initial_velocity, final_velocity)
             * 100
         )
-    if velocity_difference_percentage > acceptable_difference:
-        return acceptable_difference - velocity_difference_percentage
 
-    return 0  # No penalty if within acceptable bounds
-
-
-# Combine penalties
-def total_penalty(x):
-    return penalty_energy(x) + penalty_velocity(x)
+    # Constraint is satisfied if the difference is less than or equal to the acceptable limit
+    return acceptable_difference - abs(velocity_difference)
 
 
-# Define the penalty functions using mystic's penalty method
-@quadratic_inequality(constraint_energy)
-def penalty_energy(x):
-    return 0.0
+mon = VerboseMonitor(10)
 
 
-@quadratic_inequality(constraint_velocity)
-def penalty_velocity(x):
-    return 0.0
-
-
-# Combine penalties
-def total_penalty(x):
-    return penalty_energy(x) + penalty_velocity(x)
+def custom_callback(x):
+    y = objective(x)
+    mon(x, y)
 
 
 # Initial guess
 x0 = [9, 0, -1, 2, -1, 0.55, -3.5, -1.4]
 
-# Use VerboseMonitor to get the convergence information
-mon = VerboseMonitor(10)
-
-
-# Custom callback function
-def custom_callback(x):
-    y = objective(x)  # Compute the objective function value
-    mon(x, y)  # Call the VerboseMonitor with both x and y
-
-
-# Solve the optimization problem using the custom callback
+# Solve the optimization problem using the constraints
 res = fmin(
     objective,
     x0,
-    penalty=total_penalty,  # Pass the combined penalty function
     disp=True,
-    maxiter=100,
-    callback=custom_callback,  # Use the custom callback function
+    maxiter=70,
+    callback=custom_callback,
 )
 
 print(res)
-
-# Clear output_cache to ensure fresh output for final call
 output_cache.clear()
 print(call_cli_program(res))
