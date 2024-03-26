0e789bf09174d7a65c2ab44974e2121e63b2d4fc
Jack Schedel <jackschedel@gmail.com>
2024-03-25 20:00:11 -0400

    graph append optimize, only when need graphs

--- strategy-simulation.go
+++ strategy-simulation.go
@@ -148,7 +148,9 @@ func main() {
 	var stepDistance float64 = 1 / float64(numTicks)
 	stepDistance *= totalLength
 
-	currentTickVelo := math.Abs(args[0]) + 1
+	initialVelocity := math.Abs(args[0]) + 1
+
+	currentTickVelo := initialVelocity
 	currentTickAccel := args[1]
 
 	var accelPlot plotter.XYs
@@ -211,13 +213,15 @@ func main() {
 			var currentTickEnergy = CalculateWorkDone(currentTickVelo, stepDistance, currentElevation, prevVelo, facingDirectionRadians)
 			totalEnergyUsed += currentTickEnergy
 
-			accelPlot = append(accelPlot, plotter.XY{X: track_pos, Y: currentTickAccel})
-			veloPlot = append(veloPlot, plotter.XY{X: track_pos, Y: currentTickVelo})
-			energyPlot = append(energyPlot, plotter.XY{X: track_pos, Y: totalEnergyUsed})
-			curvaturePlot = append(curvaturePlot, plotter.XY{X: track_pos, Y: currentCurvature})
-			elevPlot = append(elevPlot, plotter.XY{X: track_pos, Y: currentElevation})
-			centripetalPlot = append(centripetalPlot, plotter.XY{X: track_pos, Y: currentTickCentripetal})
-			facingDirectionPlot = append(facingDirectionPlot, plotter.XY{X: track_pos, Y: facingDirectionRadians})
+			if graphOutput {
+				accelPlot = append(accelPlot, plotter.XY{X: track_pos, Y: currentTickAccel})
+				veloPlot = append(veloPlot, plotter.XY{X: track_pos, Y: currentTickVelo})
+				energyPlot = append(energyPlot, plotter.XY{X: track_pos, Y: totalEnergyUsed})
+				curvaturePlot = append(curvaturePlot, plotter.XY{X: track_pos, Y: currentCurvature})
+				elevPlot = append(elevPlot, plotter.XY{X: track_pos, Y: currentElevation})
+				centripetalPlot = append(centripetalPlot, plotter.XY{X: track_pos, Y: currentTickCentripetal})
+				facingDirectionPlot = append(facingDirectionPlot, plotter.XY{X: track_pos, Y: facingDirectionRadians})
+			}
 
 			// if statment only needed to prevent printing final point
 			if graphOutput && colorOffsetVar/totalLength <= 1.0 {
@@ -256,14 +260,14 @@ func main() {
 		// fmt.Println(trackDrawingVelocities)
 	}
 
-	fmt.Println("Initial Velocity (m/s):", veloPlot[0].Y)
-	fmt.Println("Final Velocity (m/s):", veloPlot[len(veloPlot)-1].Y)
-	fmt.Println("Max Velocity (m/s):", maxVelo)
-	fmt.Println("Min Velocity (m/s):", minVelo)
-	fmt.Println("Max Acceleration (m/s^2):", maxAccel)
-	fmt.Println("Min Acceleration (m/s^2):", minAccel)
+	fmt.Println("Initial Velocity (m/s): ", initialVelocity)
+	fmt.Println("Final Velocity (m/s): ", currentTickVelo)
+	fmt.Println("Max Velocity (m/s): ", maxVelo)
+	fmt.Println("Min Velocity (m/s): ", minVelo)
+	fmt.Println("Max Acceleration (m/s^2): ", maxAccel)
+	fmt.Println("Min Acceleration (m/s^2): ", minAccel)
 	fmt.Println("Max Centripetal Acceleration (m/s^2): ", maxCentripetal)
 	fmt.Println("Time Elapsed (s): ", tiempo)
-	fmt.Println("Energy Consumed (J): ", energyPlot[len(energyPlot)-1].Y)
-	fmt.Println("Energy Consumption (W): ", energyPlot[len(energyPlot)-1].Y/tiempo)
+	fmt.Println("Energy Consumed (J): ", totalEnergyUsed)
+	fmt.Println("Energy Consumption (W): ", totalEnergyUsed/tiempo)
 }
