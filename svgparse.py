1ff708fe30ee46904d98b98d44b6e9b882fe2b8e
Leokipo <147666585+Leokipo@users.noreply.github.com>
2024-04-18 21:38:11 -0400

    draw track w turtle

--- svgparse.py
+++ svgparse.py
@@ -2,6 +2,8 @@ import re
 from svg.path import parse_path, Line, CubicBezier, QuadraticBezier, Arc
 from scipy.interpolate import splev, splprep
 import numpy as np
+from turtle import *
+import math
 
 svg_path_data = "M518.862,312.612C518.526,317.135 516.46,332.575 514.376,336.774C512.321,340.916 504.473,345.797 500.795,347.972C418.602,396.575 336.377,445.127 254.002,493.421C225.836,509.935 171.819,540.599 142.787,555.559C119.214,567.707 94.511,579.55 67.964,583.497C60.133,584.661 53.047,584.76 49.338,577.003C40.518,558.556 55.579,539.71 66.076,525.797C85.252,500.381 103.666,472.815 129.255,453.077C142.204,443.089 159.405,435.992 169.791,422.629C189.569,397.181 204.862,363.125 239.514,355.781C265.065,350.366 287.142,370.895 310.47,377.06C327.287,381.505 343.786,373.252 358.75,366.978C370.96,361.858 383.998,356.686 392.356,345.809C401.444,333.983 404.465,318.612 410.293,305.284C415.943,292.362 427.552,282.782 440.249,277.231C462.214,267.629 487.037,276.605 506.496,287.922C514.827,292.767 520.947,302.761 518.862,312.612Z"
 
@@ -57,8 +59,18 @@ radii_of_curvature = [
     for i in range(num_points)
 ]
 
-print(radii_of_curvature)
+# print(radii_of_curvature)
 
 segment_lengths = get_segment_details(path)
 
-print(segment_lengths)
+speed(0)
+scalar = .2
+arcLength = 2.425 * scalar
+for radius in radii_of_curvature:
+    radius *= scalar
+    if (radius < 0):
+        degrees = (arcLength/-radius)*(180/math.pi)
+    else:
+        degrees = (arcLength/radius)*(180/math.pi)
+    circle(radius, degrees)
+done()
